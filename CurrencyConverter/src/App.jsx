2a8afc8d2534994bd12a80d3529afe7980ca9aac
sahajsatani <sahajsatani2005@gmail.com>
2024-07-27 14:06:36 +0530

    Currency Converter Add

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import currency from "./hooks/currency";
+import {InputBox} from "./component";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("INR");
+  const [to, setTo] = useState("USD");
+  
+  const currencyInfo = currency(from.toUpperCase());
+  const option = Object.keys(currencyInfo);
+  console.log(currencyInfo);
+  console.log(option);
 
+  const convert = () => {
+    setConvertedAmount(Amount * currencyInfo[to]);
+  };
+  const swap = () => {
+    setFrom(to);
+    setTo(temp);
+    setConvertedAmount(Amount);
+    setAmount(convertedAmount);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('CurrencyConverter/src/component/index.js')`,
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={Amount}
+                  currencyOption={option}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  onCurrencyChange={(currency) => setAmount(currency)}
+                  selectCurrency={from}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOption={option}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                  amountDisable={true}
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert {from.toUpperCase()} To {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ CurrencyConverter/src/hooks/currency.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+function currency( currency ) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://v6.exchangerate-api.com/v6/add37205c63361de01896e91/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res[conversion_rates]));
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+export default currency;
